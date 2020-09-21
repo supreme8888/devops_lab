d1ba1a412b6a1179c758d4a723cb4d8146bf6025
vvv <vvv@vvv.vvv>
2020-09-21 23:53:28 +0300

    3 tasks

---
+++ task1.py
@@ -0,0 +1,13 @@
+def checkyear():
+    if year % 400 == 0:
+        return(True)
+    elif year % 100 == 0:
+        return(False)
+    elif year % 4 == 0:
+        return(True)
+    else:
+        return(False)
+
+
+year = int(input("Input Year for checking: "))
+print(checkyear())
