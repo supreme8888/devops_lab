fb5d5b5fa744f4389c8ac1fd3f932c4922898652
vvv <vvv@vvv.vvv>
2020-09-21 23:52:26 +0300

    first

--- task2.py
+++
@@ -1,8 +0,0 @@
-a = input("Input first numbers sequence divided by spaces:").split()
-for i in range(len(a)):
-    a[i] = int(a[i])
-b = input("Input second numbers sequence divided by spaces:").split()
-for i in range(len(b)):
-    b[i] = int(b[i])
-
-print(sorted(list(set(a) & set(b))))
